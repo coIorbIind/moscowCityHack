c852fcc355c4468e733f9eb7ae0bc95c3d0f828a
coIorbIind <labinfpin13@gmail.com>
2022-06-11 16:08:31 +0300

    Added serializers
    Created serializer class for the article model.

---
+++ news_api/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework.fields import DateTimeField
+
+from .models import Article
+
+from rest_framework import serializers
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    published_at = DateTimeField(format="%Y-%m-%d %H:%M:%S", input_formats=("%Y-%m-%d %H:%M:%S",))
+
+    class Meta:
+        model = Article
+        fields = ("article_id", "title", "full_text", "url", "published_at")
+        read_only_fields = ("url", )
